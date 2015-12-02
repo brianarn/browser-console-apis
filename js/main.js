e5c224b00fbf59b89eca49b964264378fc13f56a
Brian Arnold <brianarn@gmail.com>
2015-12-01 21:55:44 -0700

    WIP

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+// This isn't pretty, but it doesn't really need to be
+(function main () {
+    function getData () {
+        var url = '../data/console-apis.json';
+        console.log('Getting data at ' + url);
+        return $.ajax(url, { dataType: 'json' });
+    }
 
+    function processData(data) {
+        console.log('Processing data');
+        console.log(data);
+
+        var $table = $('#console-apis');
+        var $tr;
+
+        _.forEach(console, function (item) {
+            console.log(item);
+        });
+    }
+
+    getData().then(processData);
+})();
